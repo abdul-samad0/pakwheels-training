3b86029d59d3163133affb3138d6789640aa810c
abdul-samad0 <abduls670123@gmail.com>
2026-03-24 12:41:33 +0500

    refactor: fix the folder structure

--- pakwheels/config/settings/__init__.py
+++
@@ -1,1 +0,0 @@
-# Settings package: use config.settings.dev or config.settings.prod via DJANGO_SETTINGS_MODULE
--- pakwheels/config/settings/prod.py
+++
@@ -1,6 +0,0 @@
-"""Production settings: debug off, use env for SECRET_KEY and ALLOWED_HOSTS."""
-from django.core.exceptions import ImproperlyConfigured
-
-from .base import *
-
-DEBUG = False
